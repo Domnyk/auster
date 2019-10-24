bd93b70ddd55bcd8b026e5b74f1bb8df8aa132a0
gala377 <r.galczak@gmail.com>
2019-10-24 11:14:40 +0200

    [Eurus] Rest Api refactoring

--- eurus/src/main.rs
+++ eurus/src/main.rs
@@ -1,35 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-use rocket::{
-    get, 
-    routes,
-};
-#[macro_use] extern crate diesel;
-use diesel::prelude::*;
+use rocket::routes;
 use dotenv;
+#[macro_use] extern crate diesel;
 
 pub mod db;
-
-#[get("/")]
-fn index(db: db::Connection) -> String {
-    let mut res = String::from("Writing users: \n");
-    let users = get_all_users(&db);
-    for user in users {
-        res += &format!("user {}: {}\n", user.id, user.token);
-    }
-    res
-}
-
-fn get_all_users(db: &db::Connection) -> Vec<db::models::User> {
-    use db::schema::users::dsl::*;
-    let sql_db: &diesel::SqliteConnection = &*db;
-    let result = users.load::<db::models::User>(sql_db).expect("Error loading posts");
-    result
-}  
+pub mod rest_api;
 
 fn main() {
     dotenv::dotenv().ok();
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![rest_api::index])
         .attach(db::Connection::fairing())
         .launch();
 }
