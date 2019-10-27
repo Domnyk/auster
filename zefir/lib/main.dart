f10a9c28cddb61f9563d46cf76ed26eec859e8eb
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-10-27 21:15:00 +0100

    [Zefir] Add screen with list of rooms

--- zefir/lib/main.dart
+++ zefir/lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:zefir/repo_list.dart';
+import 'package:zefir/room_list.dart';
 import 'package:zefir/services/github.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final client = Github();
-
     return GraphQLProvider(
-      client: Github().client,
-      child: MaterialApp(
-        title: 'EGO mobile',
-        theme: ThemeData(
-          primarySwatch: Colors.blue
-        ),
-        home: MyHomePage(title: 'EGO mobile'),
-      )
-    );
+        client: Github().client,
+        child: MaterialApp(
+          title: 'EGO mobile',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: MyHomePage(title: 'EGO mobile'),
+        ));
   }
 }
 
@@ -37,21 +31,16 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: RaisedButton(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: RaisedButton(
           child: Text('Przejdź do listy repozytoriów!'),
           onPressed: () {
             Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RepoList())
-            );
+                context, MaterialPageRoute(builder: (context) => RoomList()));
           },
-        )
-
-      )
-    );
+        )));
   }
 }
