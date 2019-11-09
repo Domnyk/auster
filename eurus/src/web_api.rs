372c32f0e027f3258a11296bf8d74de629ebea72
gala377 <r.galczak@gmail.com>
2019-11-09 12:53:10 +0100

    [eurus][BROKEN] fixed broken api after db shcema migration

--- eurus/src/web_api.rs
+++ eurus/src/web_api.rs
@@ -1,6 +1,3 @@
-#[cfg(debug_assertions)]
-use diesel::prelude::*;
-
 use rocket::{
     get,
     post,
@@ -10,35 +7,12 @@ use rocket::{
 #[cfg(debug_assertions)]
 use rocket::response;
 
-#[cfg(debug_assertions)]
-use crate::db;
 use crate::graphql;
 
 // TODO! Smth not working here
 
-#[cfg(debug_assertions)]
-#[get("/")]
-pub fn index(db: db::Connection) -> String {
-    let mut res = String::from("Writing users: \n");
-    let users = get_all_users(&db);
-    for user in users {
-        res += &format!("user {}: {}\n", user.id, user.token);
-    }
-    res
-}
-
-#[cfg(not(debug_assertions))]
 #[get("/")]
 pub fn index() {}
-
-#[cfg(debug_assertions)]
-fn get_all_users(db: &db::Connection) -> Vec<db::models::User> {
-    use db::schema::users::dsl::*;
-    let sql_db: &diesel::SqliteConnection = &*db;
-    users
-        .load::<db::models::User>(sql_db)
-        .expect("Error loading posts")
-}
 
 
 #[cfg(debug_assertions)]
