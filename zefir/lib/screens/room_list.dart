6807ae6f3219766bdd98c7b7e12b0c7c7361abdc
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-12-30 23:15:26 +0100

    Add speed dial

--- zefir/lib/screens/room_list.dart
+++ zefir/lib/screens/room_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:zefir/model/room_preview.dart';
 import 'package:zefir/screens/join_room.dart';
+import 'package:zefir/screens/new_room.dart';
 import 'package:zefir/widgets/room_preview_widget.dart';
 
 class RoomList extends StatelessWidget {
@@ -15,8 +17,9 @@ class RoomList extends StatelessWidget {
         title: Text('Lista pokojów, w których sie znajdujesz'),
       ),
       body: _buildList(ctx, this._rooms),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _navigateToJoinRoomScreen(ctx),
+      floatingActionButton: SpeedDial(
+        child: Icon(Icons.menu),
+        children: [_buildJoinRoomIcon(ctx), _buildNewRoomIcon(ctx)],
       ),
     );
   }
@@ -30,8 +33,23 @@ class RoomList extends StatelessWidget {
         });
   }
 
-  void _navigateToJoinRoomScreen(BuildContext ctx) {
-    Navigator.push(
-        ctx, MaterialPageRoute(builder: (BuildContext context) => JoinRoom()));
+  SpeedDialChild _buildJoinRoomIcon(BuildContext ctx) {
+    return SpeedDialChild(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        label: 'Załóż nowy pokój',
+        labelStyle: TextStyle(fontSize: 18.0),
+        onTap: () => Navigator.push(ctx,
+            MaterialPageRoute(builder: (BuildContext context) => NewRoom())));
+  }
+
+  SpeedDialChild _buildNewRoomIcon(BuildContext ctx) {
+    return SpeedDialChild(
+        child: Icon(Icons.arrow_forward_ios),
+        backgroundColor: Colors.black,
+        label: 'Dołącz do istniejącego pokoju',
+        labelStyle: TextStyle(fontSize: 18.0),
+        onTap: () => Navigator.push(ctx,
+            MaterialPageRoute(builder: (BuildContext context) => JoinRoom())));
   }
 }
