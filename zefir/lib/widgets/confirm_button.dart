66fffb5e513b22cbac3dc960024333842d0321fd
Dominik Trusinski <dominik.trusinski@gmail.com>
2020-01-18 13:15:36 +0100

    Dedicated confirm button

--- zefir/lib/widgets/confirm_button.dart
+++ zefir/lib/widgets/confirm_button.dart
@@ -4,21 +4,29 @@ import 'package:flutter/widgets.dart';
 class ConfirmButton extends StatelessWidget {
   final String _text;
   final _onPressed;
+  final bool _isRaised;
 
-  const ConfirmButton({@required String text, @required onPressed})
+  const ConfirmButton(
+      {@required String text, @required onPressed, bool isRaised = true})
       : _text = text,
-        _onPressed = onPressed;
+        _onPressed = onPressed,
+        _isRaised = isRaised;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: RaisedButton(
-        child: Text(_text),
-        onPressed: () => _onPressed(),
-        color: Colors.green,
-        textColor: Colors.white,
-      ),
-      width: double.infinity,
-    );
+    final btn = _isRaised
+        ? RaisedButton(
+            child: Text(_text),
+            onPressed: () => _onPressed(),
+            color: Colors.green,
+            textColor: Colors.white,
+          )
+        : FlatButton(
+            child: Text(_text),
+            onPressed: () => _onPressed(),
+            color: Colors.green,
+            textColor: Colors.white);
+
+    return SizedBox(child: btn);
   }
 }
