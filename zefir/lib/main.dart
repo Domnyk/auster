0abfabf185fe9fa67d17b680089d6d77625f1b72
Dominik Trusinski <dominik.trusinski@gmail.com>
2020-01-21 21:57:52 +0100

    Remove unnecessary ChangeValueNotifier

--- zefir/lib/main.dart
+++ zefir/lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:zefir/model/user.dart';
 import 'package:zefir/zefir.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => new User(),
-      child: Zefir(),
-    ),
-  );
+  runApp(Zefir());
 }
