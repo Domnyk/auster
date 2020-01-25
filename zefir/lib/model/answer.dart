b48e8488f1e11765c31cbb5012f493f03518c194
Dominik Trusinski <dominik.trusinski@gmail.com>
2020-01-25 01:00:45 +0100

    Parse Answer

--- zefir/lib/model/answer.dart
+++ zefir/lib/model/answer.dart
@@ -1,11 +1,19 @@
 class Answer {
   final int id;
   final String content;
+  final int playerToken;
 
-  Answer(this.id, this.content);
+  Answer(this.id, this.content, this.playerToken);
 
   Answer.fromGraphQl(dynamic data)
       : id = int.parse(data['id'].toString(),
             radix: 10), // TODO: Without toStirng() it throws exception. WTF?
-        content = data['content'];
+        content = data['content'],
+        playerToken = null;
+
+  Answer.fromGraphQlWithPlayerToken(dynamic data)
+      : id = int.parse(data['id'].toString(),
+            radix: 10), // TODO: Without toStirng() it throws exception. WTF?
+        content = data['content'],
+        playerToken = int.parse(data['player']['token'].toString(), radix: 10);
 }
